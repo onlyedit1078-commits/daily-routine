13ea828a6f5e2578105d1b015673819b8006211f
onlyedit1078-commits <onlyedit1078@gmail.com>
2026-08-18 17:56:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+// Calendar button
+const calendarButton = document.getElementById("calendarBtn");
+
+if (calendarButton) {
+  calendarButton.addEventListener("click", function () {
+    alert("Calendar page open hoga");
+  });
+}
+
+// Settings button
+const settingsButton = document.getElementById("settingsBtn");
+
+if (settingsButton) {
+  settingsButton.addEventListener("click", function () {
+    alert("Settings page open hoga");
+  });
+}
